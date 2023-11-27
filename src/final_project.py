f6a4d6e2e4d85069df8e306504f2130f77a8d69f
KeLi_Lumampao <kel230001@utdallas.edu>
2023-11-27 17:17:01 -0600

    trying_artist_variable

--- src/final_project.py
+++ src/final_project.py
@@ -2,19 +2,25 @@ import pygame
 import random 
 #can I import more than one of these...?
 
-def character_type():
+
+
+def character_type(artist):
 
     print("Would you like to randomize or input type of character?")
 
-def character_gender():
+def character_gender(artist):
     """
     :return: gender
     :rtype: str
     """
-    gender = ["male","female"]
-    idx = random.randrange(2)
-    return gender[idx]
     print("Would you like to randomize or input gender?")
+    if (artist == "yes")
+        gender = ["male","female"]
+        idx = random.randrange(2)
+        return gender[idx]
+    elif (artist == "no")
+    
+
 
 def character_trait():
     prompt number
