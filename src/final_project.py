a7e1a994fc1a4e150ab8408d894dc5d3a6c84b8b
KeLi_Lumampao <kel230001@utdallas.edu>
2023-11-27 16:52:42 -0600

    Testing_GIT

---
+++ src/final_project.py
@@ -0,0 +1,19 @@
+import pygame
+
+def character_type():
+
+    print("Would you like to randomize or input type of character?")
+
+def character_gender():
+
+    print("Would you like to randomize or input gender?")
+
+def character_trait():
+    prompt number
+    output that number
+    if then
+
+    print("How many personality traits will your character have? Type answer.")
+
+def character_trademark()
+    
